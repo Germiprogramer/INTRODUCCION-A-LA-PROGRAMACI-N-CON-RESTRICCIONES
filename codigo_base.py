29b94ef5f2b1ec3c5d8a2aea9cefb38e270a6028
Germiprogramer <llorentemunozgerman@gmail.com>
2023-09-24 16:52:50 +0200

    commit changes

--- codigo_base.py
+++ codigo_base.py
@@ -5,46 +5,56 @@ model = cp_model.CpModel()
 solver = cp_model.CpSolver()
 
 # 1. Variable
-army = model.NewIntVar(1, 2000, 'jugadores')
+# Creamos una variable llamada "equipo" que representa el número de jugadores.
+# La variable puede tener un valor entre 1 y 2000.
+equipo = model.NewIntVar(1, 2000, 'jugadores')
 
 # 2. Restricciones
-
-model.AddModuloEquality(0, army, 5)
-model.AddModuloEquality(0, army, 7)
-model.AddModuloEquality(0, army, 11)
+# Establecemos tres restricciones en relación al número de jugadores:
+# - El número de jugadores debe ser divisible por 5.
+# - El número de jugadores debe ser divisible por 7.
+# - El número de jugadores debe ser divisible por 11.
+model.AddModuloEquality(0, equipo, 5)
+model.AddModuloEquality(0, equipo, 7)
+model.AddModuloEquality(0, equipo, 11)
 
 # Encontrar la variable que favorece dichas restricciones
+# Intentamos encontrar un valor para "equipo" que cumpla con las restricciones.
 status = solver.Solve(model)
 
-# If a solution has been found, print results
-# Find the variable that satisfies these constraints
-status = solver.Solve(model)
+# Si se ha encontrado una solución, imprimimos los resultados
 
-# If a solution has been found, print results
+# Definimos una clase de callback para imprimir cada solución.
 class PrintSolutions(cp_model.CpSolverSolutionCallback):
-    """Callback to print every solution."""
     def __init__(self, variable):
         cp_model.CpSolverSolutionCallback.__init__(self)
         self.__variable = variable
+
     def on_solution_callback(self):
         print(self.Value(self.__variable))
 
-solution_printer = PrintSolutions(army)
+# Creamos una instancia de la clase de callback
+solution_printer = PrintSolutions(equipo)
+
+# Configuramos el solucionador para encontrar todas las soluciones posibles.
 solver.parameters.enumerate_all_solutions = True
-print("Las posibles soluciones a nuestro problema son: ")
+
+# Imprimimos todas las soluciones posibles.
+print("Las posibles soluciones a nuestro problema son:")
 status = solver.Solve(model, solution_printer)
 
-# número mínimo de jugadores
-model.Minimize(army)
-
+# Encontrar el número mínimo de jugadores
+# Minimizamos la variable "equipo" para encontrar el número mínimo de jugadores.
+model.Minimize(equipo)
 status = solver.Solve(model)
 
-print('Número mínimo de jugadores: ', solver.Value(army))
+# Imprimimos el número mínimo de jugadores encontrado.
+print('Número mínimo de jugadores:', solver.Value(equipo))
 
-
-# número máximo de jugadores
-model.Maximize(army)
-
+# Encontrar el número máximo de jugadores
+# Maximizamos la variable "equipo" para encontrar el número máximo de jugadores.
+model.Maximize(equipo)
 status = solver.Solve(model)
 
-print('Número máximo de jugadores: ', solver.Value(army))
+# Imprimimos el número máximo de jugadores encontrado.
+print('Número máximo de jugadores:', solver.Value(equipo))
